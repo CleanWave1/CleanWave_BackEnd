8ed43b0defe1bdbdda428bc23b62d9f0e580189b
Juan Diego Huaman Monzón <u202123965@upc.edu.pe>
2024-06-28 09:30:24 -0500

    feat(app): Update AppDbContext.cs, appsettings.json and Program.cs

--- CleanWave_Backend/Program.cs
+++ CleanWave_Backend/Program.cs
@@ -1,11 +1,58 @@
-var builder = WebApplication.CreateBuilder(args);
+using CleanWave_Backend.Profiles.Application.Internal.CommandServices;
+using CleanWave_Backend.Profiles.Application.Internal.QueryServices;
+using CleanWave_Backend.Profiles.Domain.Repositories;
+using CleanWave_Backend.Profiles.Domain.Services;
+using CleanWave_Backend.Profiles.Infraestructure.Persistence.EFC.Repositories;
+using CleanWave_Backend.Shared.Domain.Repositories;
+using CleanWave_Backend.Shared.Infraestructure.Persistence.EFC.Configuration;
+using CleanWave_Backend.Shared.Infraestructure.Persistence.EFC.Repositories;
+using CleanWave_Backend.Shared.Interfaces.ASP.Configuration;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers(options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableDetailedErrors();
+    });
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Shared Bounded Context Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Customer Bounded Context Injection Configuration
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerQueryService, CustomerQueryService>();
+builder.Services.AddScoped<ICustomerCommandService, CustomerCommandServices>();
+
+// Cleaning Entrepreneur Bounded Context Injection Configuration
+builder.Services.AddScoped<ICleaningEntrepreneurRepository, CleaningEntrepreneurRepository>();
+builder.Services.AddScoped<ICleaningEntrepreneurQueryService, CleaningEntrepreneurQueryService>();
+builder.Services.AddScoped<ICleaningEntrepreneurCommandService, CleaningEntrepreneurCommandService>();
+
 var app = builder.Build();
+
+// Verify Database Objects are created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,30 +62,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- CleanWave_Backend/Shared/Infraestructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ CleanWave_Backend/Shared/Infraestructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using CleanWave_Backend.Shared.Infraestructure.Persistence.EFC.Configuration.Extensions;
+﻿using CleanWave_Backend.Profiles.Domain.Model.Aggregates;
+using CleanWave_Backend.Shared.Infraestructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,32 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        base.OnModelCreating(builder);
+        //Profiles Context
+        builder.Entity<Customer>().HasKey(c => c.Id);
+        builder.Entity<Customer>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Customer>().Property(c => c.Name).IsRequired().HasColumnName("Name");
+        builder.Entity<Customer>().Property(c => c.LastName).IsRequired().HasColumnName("LastName");
+        builder.Entity<Customer>().Property(c => c.Email).IsRequired().HasColumnName("Email");
+        builder.Entity<Customer>().Property(c => c.Phone).IsRequired().HasColumnName("Phone");
+        builder.Entity<Customer>().OwnsOne(c => c.Space, s =>
+        {
+            s.WithOwner().HasForeignKey("Id");
+            s.Property(p => p.PropertyType).HasColumnName("PropertyType");
+            s.Property(p => p.CleaningType).HasColumnName("CleaningType");
+            s.Property(p => p.SpaceSize).HasColumnName("SpaceSize");
+            s.Property(p => p.NumberRooms).HasColumnName("NumberRooms");
+            s.Property(p => p.FloorType).HasColumnName("FloorType");
+            s.Property(p => p.Instructions).HasColumnName("Instructions");
+        });
+        
+        //Cleaning Entrepreneur Context
+        builder.Entity<CleaningEntrepreneur>().HasKey(c => c.Id);
+        builder.Entity<CleaningEntrepreneur>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<CleaningEntrepreneur>().Property(c => c.Name).IsRequired().HasColumnName("Name");
+        builder.Entity<CleaningEntrepreneur>().Property(c => c.Email).IsRequired().HasColumnName("Email");
+        builder.Entity<CleaningEntrepreneur>().Property(c => c.Username).IsRequired().HasColumnName("Username");
+        
         // Apply SnakeCase Naming Convention
         builder.UseSnakeCaseWithPluralizedTableNamingConvention();
     }
